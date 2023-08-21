8900fc23d38b623c8db6ba541fc933b6ca788091
PLK <unrecht.official@gmail.com>
2023-08-21 22:40:13 +0200

    🎉 Initial commit.

---
+++ src/main/java/de/plk/plugin/LobbyPlugin.java
@@ -0,0 +1,23 @@
+package de.plk.plugin;
+
+import de.spicysip.core.api.AbstractSpicySpigot;
+
+/**
+ * @author SoftwareBuilds
+ * @since 21.08.2023 22:36
+ * Copyright © 2023 | SoftwareBuilds | All rights reserved.
+ */
+public final class LobbyPlugin extends AbstractSpicySpigot {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+    }
+}
